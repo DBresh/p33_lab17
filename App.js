c7bc92a467081a99d8de709e36ea0a53e72f543d
DBresh <lenovo.dmitro@gmail.com>
2023-05-03 16:55:13 +0300

    comm

--- App.js
+++ App.js
@@ -3,33 +3,6 @@ import TestState from "./components/TestState";
 
 
 function App() {
-  const usersIt = [
-    {
-      content: "Frontend Misha",
-      id: "fe",
-      relations: ["pm", "be", "ds"],
-    },
-    {
-      content: "Design Pasha",
-      id: "ds",
-      relations: ["pm", "fe"],
-    },
-    {
-      content: "Project Manager Sanya",
-      id: "pm",
-      relations: ["ba", "fe", "be", "ds"],
-    },
-    {
-      content: "Backend Grisha",
-      id: "be",
-      relations: ["pm", "fe", "ba"],
-    },
-    {
-      content: "Business Analyst Sveta",
-      id: "ba",
-      relations: ["pm"],
-    },
-  ];
 
   return (
     <div className="App">
